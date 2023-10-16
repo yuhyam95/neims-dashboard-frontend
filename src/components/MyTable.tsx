2e0ef8343b27d9a461659307dcbb6e3cacee915e
Yusuf Habu <yuhyam95@gmail.com>
2023-10-16 12:39:06 +0100

    Added Table Rows

--- src/components/MyTable.tsx
+++ src/components/MyTable.tsx
@@ -1,7 +1,8 @@
-import { Table, Thead, Tbody, Tr, Th, TableContainer, Td, Tfoot } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, TableContainer } from "@chakra-ui/react";
+import data from '../constants/mockData'
+import TableRows from "./TableRows";
 
 function MyTable() {
-
   return (
   <TableContainer bg="white" borderRadius="10px">
   <Table size='lg'>
@@ -15,6 +16,17 @@ function MyTable() {
       </Tr>
     </Thead>
     <Tbody>
+     
+        {/* {data.map((data, index) => (
+          <TableRows
+            key={index}
+            name={data.name}
+            station={data.station}
+            reason={data.reason}
+            quantity={data.quantity}
+            date={data.date}
+          />
+        ))} */}
     </Tbody>
   </Table>
 </TableContainer>
