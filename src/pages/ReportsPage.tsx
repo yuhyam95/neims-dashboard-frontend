6914d0658ebe8f83b6360d9b9c71910079aca2e0
Yusuf Habu <yuhyam95@gmail.com>
2023-12-18 14:35:59 +0100

    Filtering disaster reports based on User Role

--- src/pages/ReportsPage.tsx
+++ src/pages/ReportsPage.tsx
@@ -1,27 +1,29 @@
-import { HStack, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react"
+import { HStack, Heading } from "@chakra-ui/react"
 import ReportsTable from "../components/ReportsTable"
-import CreateReport from "../components/CreateReport";
 import useReports from "../hooks/useReports";
+import { useAuth } from "../context/AuthContext";
 
 const ReportsPage = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const { reports } = useReports('')
+  const {user} = useAuth();
+  const userRole = user?.role.name;
+  const userStation = user?.station.name;
+
+  let queryParams = {}
+  if (userRole == 'Head-officer') {
+    queryParams = {approved: true}
+  }
+  else{
+    queryParams = '';
+  }
+  const { reports } = useReports(queryParams)
+  
+
   return (
     <>
     <HStack justify='space-between'>
     <Heading mb={4} ml={4}> Reports </Heading>
     </HStack>
     <ReportsTable reports={reports}/>
-    <Modal isOpen={isOpen} onClose={onClose} size='4xl'>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Create a Report</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-          <CreateReport />
-          </ModalBody>
-        </ModalContent>
-      </Modal>
     </>
   )
 }
